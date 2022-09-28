b466548792593556ed4c73ddfc5e1e8c0c2ee4d4
Tatsuyuki Ishi <ishitatsuyuki@google.com>
2022-09-28 16:40:09 +0900

    wip

--- src/scalar.rs
+++ src/scalar.rs
@@ -1,47 +1,76 @@
+use std::arch::asm;
 use crate::Table;
 
 #[inline]
-pub(crate) fn next_match(hash: &mut u64, table: &Table, buf: &[u8], mask: u64) -> Option<usize> {
-    for (i, b) in buf.iter().enumerate() {
-        *hash = (*hash << 1).wrapping_add(table[*b as usize]);
+fn add_sh(a: u64, b: u64) -> u64 {
+    let out: u64;
+    unsafe {
+        #[cfg(target_arch = "x86_64")]
+        asm!("lea {0}, [{1} + {2} * 2]", lateout(reg) out, in(reg) b, in(reg) a);
+        #[cfg(target_arch = "aarch64")]
+        asm!("add {0}, {1}, {2}, lsl #1", lateout(reg) out, in(reg) b, in(reg) a);
+    }
+    out
+}
 
-        if *hash & mask == 0 {
+#[inline]
+fn add_sh2(a: u64, b: u64) -> u64 {
+    let out: u64;
+    unsafe {
+        asm!("lea {0}, [{1} + {2} * 4]", lateout(reg) out, in(reg) b, in(reg) a);
+        #[cfg(target_arch = "aarch64")]
+        asm!("add {0}, {1}, {2}, lsl #2", lateout(reg) out, in(reg) b, in(reg) a);
+    }
+    out
+}
+
+#[inline]
+pub fn next_match(hash: &mut u64, table: &Table, buf: &[u8], mask: u64) -> Option<usize> {
+    let mut hash_ = *hash;
+    for (i, b) in buf.iter().enumerate() {
+        hash_ = add_sh(hash_, table[*b as usize]);
+
+        if hash_ & mask == 0 {
+            *hash = hash_;
             return Some(i + 1);
         }
     }
 
+    *hash = hash_;
     None
 }
 
 #[inline]
-pub fn next_match2(hash: &mut u64, table: &Table, table_ls: &Table, buf: &[u8], mask: u64) -> Option<usize> {
-    if mask >> 63 == 1 {
-        return next_match(hash, table, buf, mask);
-    }
-    let mask_ls = mask << 1;
+pub fn next_match2(hash: &mut u64, table: &Table, buf: &[u8], mask: u64) -> Option<usize> {
+    let mut hash_ = *hash;
     let len = buf.len();
     for i in 0..len / 2 {
-        let hash_orig = *hash;
-        let b1 = unsafe { table_ls[*buf.get_unchecked(i * 2) as usize] };
+        let hash_orig = hash_;
         let b2 = unsafe { table[*buf.get_unchecked(i * 2 + 1) as usize] };
-        *hash = (*hash << 2).wrapping_add(b1);
-        if *hash & mask_ls == 0 {
-            *hash = (hash_orig << 1).wrapping_add(unsafe { table[*buf.get_unchecked(i * 2) as usize] });
+        let b1 = unsafe { table[*buf.get_unchecked(i * 2) as usize] };
+
+        hash_ = add_sh(hash_orig, b1);
+        let b1b2 = add_sh(b1, b2);
+        if hash_ & mask == 0 {
+            *hash = hash_;
             return Some(i * 2 + 1);
         }
-        *hash = hash.wrapping_add(b2);
-        if *hash & mask == 0 {
+        hash_ = add_sh2(hash_orig, b1b2);
+        if hash_ & mask == 0 {
+            *hash = hash_;
             return Some(i * 2 + 2);
         }
     }
     if len % 2 == 1 {
-        *hash = (*hash << 1).wrapping_add(unsafe { table[*buf.get_unchecked(len - 1) as usize] });
+        hash_ = (hash_ << 1).wrapping_add(unsafe { table[*buf.get_unchecked(len - 1) as usize] });
 
-        if *hash & mask == 0 {
+        if hash_ & mask == 0 {
+            *hash = hash_;
             return Some(len);
         }
     }
 
+    *hash = hash_;
     None
 }
 
@@ -63,7 +92,7 @@ mod tests {
             let mut offset = 0;
             while offset < 10240 {
                 let result_scalar = crate::scalar::next_match(&mut hash1, &DEFAULT_TABLE, &bytes[offset..], mask);
-                let result_accelx = next_match2(&mut hash2, &DEFAULT_TABLE, &DEFAULT_TABLE_LS, &bytes[offset..], mask);
+                let result_accelx = next_match2(&mut hash2, &DEFAULT_TABLE, &bytes[offset..], mask);
 
                 match (result_scalar, result_accelx) {
                     (Some(a), Some(b)) => {
@@ -98,6 +127,6 @@ fn throughput(b: &mut test::Bencher) {
 #[bench]
 fn throughput2(b: &mut test::Bencher) {
     crate::bench::throughput(b, |hash, buf, mask| {
-        next_match2(hash, &crate::DEFAULT_TABLE, &crate::DEFAULT_TABLE_LS, buf, mask)
+        next_match2(hash, &crate::DEFAULT_TABLE, buf, mask)
     })
 }
